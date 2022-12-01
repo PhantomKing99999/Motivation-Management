e14530832d5f5db15c6f2fd81f7f023df5b54c48
PhantomKing99999 <LiuQinAnAndy@gmail.com>
2022-12-01 14:18:50 +0800

    Andy is gay and homophobic

--- Motivation Management/ContentView.swift
+++ Motivation Management/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Elbert Ainstein Sucks Dick like a Champ")
             .padding()
     }
 }
