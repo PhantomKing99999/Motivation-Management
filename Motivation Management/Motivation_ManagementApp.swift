6bbb37993da79c56ad06d603b6355b205ce77266
PhantomKing <LiuQinAnAndy@gmail.com>
2022-12-01 14:07:49 +0800

    Initial Commit

---
+++ Motivation Management/Motivation_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Motivation_ManagementApp.swift
+//  Motivation Management
+//
+//  Created by PhantomKing on 1/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Motivation_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
